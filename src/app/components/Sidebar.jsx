1e539a72221dbdd3b6c10290e1450e21004bfbc5
AllanKariuki <a.kariuki@keshiholdings.com>
2025-05-21 08:25:46 +0300

    feat: function to fetch conversations

--- src/app/components/Sidebar.jsx
+++ src/app/components/Sidebar.jsx
@@ -1,4 +1,18 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import axios from 'axios';
+
 const Sidebar = () => {
+    const [conversations, setConversations] = useState([]);
+    
+    const [ loading, setLoading ] = useState(false);
+
+    const fetchConversations = useCallback(async () => {
+        setLoading(true);
+        try {
+            const response = await axios.
+        }
+    }, []); 
+
     return (
         <div className="absolute p-3 bg-gray-80 text-gray-800 w-[15vw]">
             <ul className="space-y-2 flex flex-col">
