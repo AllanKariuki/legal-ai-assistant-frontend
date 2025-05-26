ff4b63ac9a41f7095cacbdad2d69d9428c77cecc
AllanKariuki <a.kariuki@keshiholdings.com>
2025-05-26 09:18:51 +0300

    refactor: change breakpoint back to md

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,7 +1,7 @@
 import { AlignLeft } from "lucide-react";
 
 const Header = ({ newChatClick }) => (
-  <div className="flex items-center justify-between p-4 block lg:hidden">
+  <div className="flex items-center justify-between p-4 block md:hidden">
     <button className="flex items-center gap-2 bg-gray-200 spce-x-2 rounded-full px-4 py-3 hover:cursor-pointer" onClick={newChatClick}>
       <AlignLeft className="w-5 h-5 text-gray-500" />
       <span className="text-black">New chat</span>
